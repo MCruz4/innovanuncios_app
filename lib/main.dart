9576f95fc05c004c668b3d10064e982ae5a7c759
Mauricio Cruz <m97cruz@gmail.com>
2020-04-02 23:46:23 -0600

    Creacion de Proyecto App Flutter

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:innovanuncios_app/dashboard.dart';
+
+void main() => runApp(Dashboard());
+
+/*Conservar esto, para evaluar si hay una sesion existente. sino, mandar al login, de lo contrario, mandar al Dashboard*/
