9051778d4391af81a6bd0e38729f69a8f2cbba33
jimmy-ugb <58155232+jimmy-ugb@users.noreply.github.com>
2020-04-26 12:36:26 -0600

    Agregando login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:innovanuncios_app/src/views/dashboard.dart';
+import 'package:innovanuncios_app/src/views/login.dart';
+
 //import 'package:innovanuncios_app/signup.dart';
-void main() => runApp(Dashboard());
+void main() => runApp(LoginPage());
 
 /*Conservar esto, para evaluar si hay una sesion existente. sino, mandar al login, de lo contrario, mandar al Dashboard*/
